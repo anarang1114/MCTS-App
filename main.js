6a57ac5573f5714cd437470508eb3d2f8a1f32e2
jssoni14 <jsoni14@gmail.com>
2018-11-30 12:14:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,79 @@
 var proxy = 'https://cors-anywhere.herokuapp.com/';
 $ (setup);
 startlat=0;
-startlng=0; 
+startlng=0;
 endlat= 0;
 endlng=0;
 startaddressdefined = false;
 
+$(document).ready(function(){
+	$('#Flipinstructions').click(function(){
+						$('#instructions').slideToggle('slow');
+						 });
+
+});
+
+
+
 function setup(){
-	//$('#instructions').hide();
+	
 	$("#getlocation").click(findmylocation);
 	$('#ShowMilwaukeeMap').click(setmilwuakeemap);
 	$('#getbestroute').click(findbestroute);
 	$('#RemoveAddress').click(clearaddressstartend);
-	
+	$('#instructions').hide();
+
+
+
+
 }
-	
+
 
 function findmylocation(){
-	$("<div/>").attr('id','map').appendTo('body');  
-	var popup = L.popup(),
-  geocode,
-  map;
 
-	
+	$("<div/>").attr('id','map').appendTo('body');
+	var popup = L.popup(),
+	geocode,
+	map;
+
 var geolocationMap = L.map('map', {
     layers: MQ.mapLayer(),
     center: [43.038902, -87.906471],
     zoom: 15
 }).on('click', function(e) {
     popup.setLatLng(e.latlng).openOn(this);
-	
+
   geocode.reverse(e.latlng);
 
 geocode.reverse(e.latlng);
 
 var latlnglength = (e.latlng).toString().length;
-  
+
   endlat = Number((e.latlng).toString().substring(7,latlnglength-1).split(",",1));
-  
+
   endlng = Number(e.latlng.toString().substring(endlat.toString().length+9, latlnglength-1));
-  
+
 });
   	geocode = MQ.geocode().on('success', function(e) {
   popup.setContent(geocode.describeLocation(e.result.best));
 });
 
 if (navigator.geolocation) {
-	
+
     navigator.geolocation.getCurrentPosition(function (position) {
         var latLng = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
-			
+
         };
 		startlat = position.coords.latitude;
 		startlng = position.coords.longitude;
-		
+
         popup.setLatLng(latLng);
         popup.setContent("This is your location");
-		
+
         popup.openOn(geolocationMap);
-		
+
         geolocationMap.setView(latLng);
 		startaddressdefined = true;
     }, function () {
@@ -80,9 +93,11 @@ function geolocationErrorOccurred(geolocationSupported, popup, latLng) {
     popup.openOn(geolocationMap);
 }
 
-}
+	}
+
+
 function setmilwuakeemap(){
-$("<div/>").attr('id','map').appendTo('body');  
+$("<div/>").attr('id','map').appendTo('body');
 var popup = L.popup(),
   geocode,
   map;
@@ -93,26 +108,26 @@ map = L.map('map', {
   zoom: 14 })
   .on('click', function(e) {
     popup.setLatLng(e.latlng).openOn(this);
-	
+
   geocode.reverse(e.latlng);
- 
+
   if(!startaddressdefined){
   var latlnglength = (e.latlng).toString().length;
-  
+
   startlat = Number((e.latlng).toString().substring(7,latlnglength-1).split(",",1));
-  
+
   startlng = Number(e.latlng.toString().substring(startlat.toString().length+9, latlnglength-1));
   startaddressdefined=true;
   }
   else{
 	  var latlnglength = (e.latlng).toString().length;
-  
+
   endlat = Number((e.latlng).toString().substring(7,latlnglength-1).split(",",1));
-  
+
   endlng = Number(e.latlng.toString().substring(endlat.toString().length+9, latlnglength-1));
   //alert("Start latlng"+ startlat + " "+startlng+ "and End latlng" + endlat + " "+ endlng);
   }
-  
+
 });
 
 geocode = MQ.geocode().on('success', function(e) {
@@ -140,11 +155,11 @@ function findbestroute(){
 		alert("No start location and end location selected");
 	}
 	else{
-		alert(typeof startlat);
+
 	var mymap = document.getElementById("map");
 	mymap.parentNode.removeChild(mymap);
 	mymap.remove();
-	$("<div/>").attr('id','map').appendTo('body'); 
+	$("<div/>").attr('id','map').appendTo('body');
 	var map,
 dir;
 
@@ -166,11 +181,11 @@ dir = MQ.routing.directions()
             maneuvers = legs[0].maneuvers;
 
             for (i=0; i < maneuvers.length; i++) {
-              
+
 				finalhtml +='<br>'+(i+1) +'. ' +maneuvers[i].narrative+'</br>';
             }
 				L.DomUtil.get('results').innerHTML = finalhtml;
-            
+
         }
     });
 
@@ -198,4 +213,3 @@ map.addLayer(MQ.routing.routeLayer({
 	*/
 
 }
-
